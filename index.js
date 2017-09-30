006b0558adac1f4c14779f03361b9298333a4c27
Nikodem Jaworski <nikodemjaworski@gmail.com>
2017-09-30 14:38:55 +0200

    Initial

---
+++ index.js
@@ -0,0 +1,15 @@
+const FSNotifier = require('fastboot-fs-notifier'),
+      FastBootAppServer = require('fastboot-app-server'),
+      distPath = 'dist';
+
+let notifier = new FSNotifier({
+  targetDir: distPath
+});
+
+let server = new FastBootAppServer({
+  notifier: notifier,
+  distPath: distPath,
+  gzip: true
+});
+
+server.start();
